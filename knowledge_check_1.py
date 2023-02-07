472a1cb29296a96a466da4496ed16ae7427fe45e
David York <davidyork1995@gmail.com>
2023-02-07 15:27:28 -0500

    Deleted unnecessary code

--- knowledge_check_1.py
+++ knowledge_check_1.py
@@ -8,19 +8,3 @@ dictionary = {
 print(dictionary["dog"])
 mealtimes = ("10:00AM", "1:00PM", "5:00PM", "9:00PM")
 print(mealtimes[0])
-
-pets = [
-    {
-    "species" : "Dog",
-    "type" : "Corgi",
-    "mealtimes" : ("10:00AM", "1:00PM", "5:00PM", "9:00PM"),
-    "Friendly before meal?" : True},
-    {
-    "species" : "Cat",
-    "type" : "Calico"
-    "mealtimes" : ("8:00AM", "11:00AM", "4:00PM", "8:00PM"),
-    "Friendly before meal?" : False
-    }
-]
-print(pets[0])
-print(pets["species"])
